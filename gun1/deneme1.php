2085515a68a101d5fc8875c16e69bd80ee5339d3
fatmarslan <fatmarslan114@gmail.com>
2015-08-08 12:39:05 +0300

    commit

---
+++ gun1/deneme1.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8"/>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 08.08.2015
+ * Time: 08:33
+ */
+$sayi1 = 5;
+$sayi2 = 3;
+// echo $sayi1 + $sayi2;
+// echo $sayi1 - $sayi2;
+// echo +sayi1 * $sayi2;
+// echo floor ($sayi1 / $sayi2); // aşağı yuvarlama
+// echo ceil($sayi1 / $sayi2); // yukarı yuvarlama
+
+// echo $sayi1 % $sayi2;
+echo $sayi2;
+
+?>
+
